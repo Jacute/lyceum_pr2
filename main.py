ae695670e418da2fd0473583323ed9e4567dd0bf
Egor Bochkarev <Eri542@yandex.ru>
2021-01-04 14:07:49 +0700

    Fix бага с анимацией jotaro_afk

--- main.py
+++ main.py
@@ -16,34 +16,18 @@ def load_image(name):
 
 class AnimaSprite(pg.sprite.Sprite):
     """Анимированный спрайт"""
-    def __init__(self, sheet, cols, rows, x, y):
+    def __init__(self, sheet, x, y):
         super().__init__(all_sprites)
         # frames - атрибут класса,
         # список для хранения последовательности кадров спрайта:
-        self.frames = []
-        # Разрезаем лист на кадры,
-        # используя функцию cut_sheet() из данного класса (см. ниже):
-        self.cut_sheet(sheet, cols, rows)
+        self.frames = sheet
         # Обнуляем номер текущего кадра:
         self.cur_frame = 0
         # image - атрибут класса,
         # в который помещаем текущий кадр:
         self.image = self.frames[self.cur_frame]
         # Помещаем прямоугольник с кадром в координаты (x, y):
-        self.rect = self.rect.move(x, y)
-
-    def cut_sheet(self, sheet, cols, rows):
-        """Функция разрезания листа с кадрами спрайта"""
-        # Задаём маленький прямоугольник размером с кадр:
-        self.rect = pg.Rect(0, 0, sheet.get_width() // cols, sheet.get_height() // rows)
-        # Пробегаем по всем кадрам спрайта:
-        for j in range(rows):
-            for i in range(cols):
-                # Определяем координаты кадра на листе:
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                # Копируем кадр из листа в список frames, используя метод subsurface(),
-                # который возвращает новую поверхность с нарисованным на ней кадром:
-                self.frames.append(sheet.subsurface(pg.Rect(frame_location, self.rect.size)))
+        self.rect = self.image.get_rect().move(x, y)
 
     def update(self):
         """Смена кадра спрайта"""
@@ -55,18 +39,18 @@ class AnimaSprite(pg.sprite.Sprite):
         self.image = self.frames[self.cur_frame]
 
     def go_right(self):
-        global x, walking
+        global x
         if walking:
-            self.rect.move(self.rect.x + 15, self.rect.y)
-            x += 15
-            pg.time.set_timer(one_step_event, 444)
+            self.rect.move(self.rect.x + 30, self.rect.y)
+            x += 30
+            pg.time.set_timer(one_step_event, 450)
 
     def go_left(self):
-        global x, walking
+        global x
         if walking:
-            self.rect.move(self.rect.x - 15, self.rect.y)
-            x -= 15
-            pg.time.set_timer(one_step_event, 444)
+            self.rect.move(self.rect.x - 30, self.rect.y)
+            x -= 30
+            pg.time.set_timer(one_step_event, 450)
 
 
 if __name__ == '__main__':
@@ -75,15 +59,17 @@ if __name__ == '__main__':
     size = width, height = 300, 300
     screen = pg.display.set_mode((size), pg.RESIZABLE)
     all_sprites = pg.sprite.Group()
-    sheet_jotaro = load_image('jotaro_afk.png')
-    walking_jotaro_right = load_image('walking_jotaro_right.png')
-    walking_jotaro_left = load_image('walking_jotaro_left.png')
+    # Загружаем спрайты
+    jotaro_afk = [load_image('jotaro_afk1.png'), load_image('jotaro_afk2.png'),
+                  load_image('jotaro_afk3.png'), load_image('jotaro_afk4.png'),
+                  load_image('jotaro_afk5.png'), load_image('jotaro_afk6.png'),
+                  load_image('jotaro_afk7.png'), load_image('jotaro_afk8.png')]
     one_step_event = pg.USEREVENT + 1
     pg.time.set_timer(one_step_event, 0)
     # Задаём координаты отрисовки спрайта в игровом окне:
     x, y = (0, 0)
     # Создаём экземпляр анимированного спрайта:
-    sprite_jotaro = AnimaSprite(sheet_jotaro, 20, 1, x, y)
+    sprite_jotaro = AnimaSprite(jotaro_afk, x, y)
     pg.key.set_repeat(200, 10)
     fps = 15
     running = True
@@ -93,7 +79,7 @@ if __name__ == '__main__':
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-            elif event.type == pg.KEYDOWN:
+            '''elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_RIGHT and not walking:
                     sprite_jotaro.kill()
                     sprite_walking_jotaro = AnimaSprite(walking_jotaro_right, 16, 1, x, y)
@@ -108,7 +94,7 @@ if __name__ == '__main__':
                 sprite_walking_jotaro.kill()
                 pg.time.set_timer(one_step_event, 0)
                 sprite_jotaro = AnimaSprite(sheet_jotaro, 20, 1, x, y)
-                walking = False
+                walking = False'''
         screen.fill(pg.Color('white'))
         all_sprites.draw(screen)
         all_sprites.update()
