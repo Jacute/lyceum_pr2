f14fdfb5dc048a1e5fef71478e536466b3c13d20
RezepovPeter <rezepov_peter@mail.ru>
2021-02-19 20:54:24 +0700

    Добавление остановки при столкновении

---
+++ load_sprites.py
@@ -0,0 +1,133 @@
+from main import load_image
+
+
+# Загружаем спрайты:
+sprite_jotaro_afk_right_side = [load_image('jotaro_afk1.png'), load_image('jotaro_afk2.png'),
+                                load_image('jotaro_afk3.png'), load_image('jotaro_afk4.png'),
+                                load_image('jotaro_afk5.png'), load_image('jotaro_afk6.png'),
+                                load_image('jotaro_afk7.png'), load_image('jotaro_afk8.png')]
+sprite_jotaro_walking_right1 = [load_image('jotaro_walking_right1.png'),
+                                load_image('jotaro_walking_right2.png'),
+                                load_image('jotaro_walking_right3.png'),
+                                load_image('jotaro_walking_right4.png'),
+                                load_image('jotaro_walking_right5.png'),
+                                load_image('jotaro_walking_right6.png'),
+                                load_image('jotaro_walking_right7.png'),
+                                load_image('jotaro_walking_right8.png')]
+sprite_jotaro_walking_right2 = [load_image('jotaro_walking_right9.png'),
+                                load_image('jotaro_walking_right10.png'),
+                                load_image('jotaro_walking_right11.png'),
+                                load_image('jotaro_walking_right12.png'),
+                                load_image('jotaro_walking_right13.png'),
+                                load_image('jotaro_walking_right14.png'),
+                                load_image('jotaro_walking_right15.png'),
+                                load_image('jotaro_walking_right16.png')]
+sprite_jotaro_walking_left1 = [load_image('jotaro_walking_left8.png'),
+                               load_image('jotaro_walking_left7.png'),
+                               load_image('jotaro_walking_left6.png'),
+                               load_image('jotaro_walking_left5.png'),
+                               load_image('jotaro_walking_left4.png'),
+                               load_image('jotaro_walking_left3.png'),
+                               load_image('jotaro_walking_left2.png'),
+                               load_image('jotaro_walking_left1.png')]
+sprite_jotaro_walking_left2 = [load_image('jotaro_walking_left16.png'),
+                               load_image('jotaro_walking_left15.png'),
+                               load_image('jotaro_walking_left14.png'),
+                               load_image('jotaro_walking_left13.png'),
+                               load_image('jotaro_walking_left12.png'),
+                               load_image('jotaro_walking_left11.png'),
+                               load_image('jotaro_walking_left10.png'),
+                               load_image('jotaro_walking_left9.png')]
+sprite_jotaro_jumping = [load_image('jotaro_jump1.png'), load_image('jotaro_jump2.png'),
+                         load_image('jotaro_jump3.png'), load_image('jotaro_jump4.png'),
+                         load_image('jotaro_jump5.png'), load_image('jotaro_jump6.png'),
+                         load_image('jotaro_jump7.png'), load_image('jotaro_jump8.png'),
+                         load_image('jotaro_jump9.png'), load_image('jotaro_jump10.png'),
+                         load_image('jotaro_jump11.png'), load_image('jotaro_jump12.png'),
+                         load_image('jotaro_jump13.png'), load_image('jotaro_jump14.png'),
+                         load_image('jotaro_jump15.png'), load_image('jotaro_jump16.png'),
+                         load_image('jotaro_jump17.png'), load_image('jotaro_jump18.png'),
+                         load_image('jotaro_jump19.png'), load_image('jotaro_jump20.png'),
+                         load_image('jotaro_jump21.png'), load_image('jotaro_jump22.png')]
+sprite_jotaro_start_sitting = [load_image('jotaro_sit1.png'), load_image('jotaro_sit2.png'),
+                               load_image('jotaro_sit3.png'), load_image('jotaro_sit4.png')]
+sprite_jotaro_sitting = [load_image('jotaro_sit5.png')]
+sprite_jotaro_end_sitting = [load_image('jotaro_sit8.png'), load_image('jotaro_sit9.png'),
+                             load_image('jotaro_sit10.png'), load_image('jotaro_sit11.png'),
+                             load_image('jotaro_sit12.png'), load_image('jotaro_sit13.png'),
+                             load_image('jotaro_sit14.png'), load_image('jotaro_sit15.png'),
+                             load_image('jotaro_sit16.png'), load_image('jotaro_sit17.png')]
+sprite_jotaro_light_attack = [load_image('jotaro_light_attack1.png'), load_image('jotaro_light_attack2.png'),
+                              load_image('jotaro_light_attack3.png'), load_image('jotaro_light_attack4.png'),
+                              load_image('jotaro_light_attack5.png'), load_image('jotaro_light_attack6.png'),
+                              load_image('jotaro_light_attack7.png'), load_image('jotaro_light_attack8.png')]
+sprite_jotaro_sitting_light_attack = [load_image('jotaro_sitting_light_attack8.png'),
+                                      load_image('jotaro_sitting_light_attack7.png'),
+                                      load_image('jotaro_sitting_light_attack6.png'),
+                                      load_image('jotaro_sitting_light_attack5.png'),
+                                      load_image('jotaro_sitting_light_attack4.png'),
+                                      load_image('jotaro_sitting_light_attack3.png'),
+                                      load_image('jotaro_sitting_light_attack2.png'),
+                                      load_image('jotaro_sitting_light_attack1.png')]
+sprite_jotaro_blocking1 = [load_image('jotaro_block1.png'), load_image('jotaro_block2.png')]
+sprite_jotaro_blocking2 = [load_image('jotaro_block3.png')]
+sprite_jotaro_blocking3 = [load_image('jotaro_block4.png'), load_image('jotaro_block5.png'),
+                           load_image('jotaro_block6.png'), load_image('jotaro_block7.png'),
+                           load_image('jotaro_block8.png'), load_image('jotaro_block9.png'),
+                           load_image('jotaro_block10.png'), load_image('jotaro_block11.png'),
+                           load_image('jotaro_block12.png')]
+sprite_dio_afk_left_side = [load_image('dio_afk1.png'), load_image('dio_afk2.png'),
+                            load_image('dio_afk3.png'), load_image('dio_afk4.png'),
+                            load_image('dio_afk5.png'), load_image('dio_afk6.png'),
+                            load_image('dio_afk7.png'), load_image('dio_afk8.png')]
+sprite_dio_walking_right1 = [load_image('dio_walking_right1.png'),
+                             load_image('dio_walking_right2.png'),
+                             load_image('dio_walking_right3.png'),
+                             load_image('dio_walking_right4.png'),
+                             load_image('dio_walking_right5.png'),
+                             load_image('dio_walking_right6.png'),
+                             load_image('dio_walking_right7.png'),
+                             load_image('dio_walking_right8.png')]
+sprite_dio_walking_right2 = [load_image('dio_walking_right9.png'),
+                             load_image('dio_walking_right10.png'),
+                             load_image('dio_walking_right11.png'),
+                             load_image('dio_walking_right12.png'),
+                             load_image('dio_walking_right13.png'),
+                             load_image('dio_walking_right14.png'),
+                             load_image('dio_walking_right15.png'),
+                             load_image('dio_walking_right16.png')]
+sprite_dio_walking_left1 = [load_image('dio_walking_left1.png'),
+                            load_image('dio_walking_left2.png'),
+                            load_image('dio_walking_left3.png'),
+                            load_image('dio_walking_left4.png'),
+                            load_image('dio_walking_left5.png'),
+                            load_image('dio_walking_left6.png'),
+                            load_image('dio_walking_left7.png'),
+                            load_image('dio_walking_left8.png')]
+sprite_dio_walking_left2 = [load_image('dio_walking_left9.png'),
+                            load_image('dio_walking_left10.png'),
+                            load_image('dio_walking_left11.png'),
+                            load_image('dio_walking_left12.png'),
+                            load_image('dio_walking_left13.png'),
+                            load_image('dio_walking_left14.png'),
+                            load_image('dio_walking_left15.png'),
+                            load_image('dio_walking_left16.png')]
+sprite_dio_jumping = [load_image('dio_jump1.png'), load_image('dio_jump2.png'),
+                      load_image('dio_jump4.png'), load_image('dio_jump5.png'),
+                      load_image('dio_jump6.png'), load_image('dio_jump7.png'),
+                      load_image('dio_jump8.png'), load_image('dio_jump9.png'),
+                      load_image('dio_jump10.png'), load_image('dio_jump11.png'),
+                      load_image('dio_jump12.png'), load_image('dio_jump13.png')]
+sprite_dio_start_sitting = [load_image('dio_sit1.png'), load_image('dio_sit2.png'),
+                            load_image('dio_sit3.png')]
+sprite_dio_sitting = [load_image('dio_sit4.png')]
+sprite_dio_end_sitting = [load_image('dio_sit5.png'), load_image('dio_sit6.png'),
+                          load_image('dio_sit7.png')]
+sprite_dio_light_attack = [load_image('dio_light_attack1.png'), load_image('dio_light_attack2.png'),
+                           load_image('dio_light_attack3.png'), load_image('dio_light_attack4.png'),
+                           load_image('dio_light_attack5.png')]
+sprite_dio_sitting_light_attack = [load_image('dio_sitting_light_attack1.png'), load_image('dio_sitting_light_attack2.png'),
+                                   load_image('dio_sitting_light_attack3.png'), load_image('dio_sitting_light_attack4.png'),
+                                   load_image('dio_sitting_light_attack5.png')]
+sprite_hp_and_mana_jotaro = load_image("hp_and_mana_jotaro.png")
+sprite_hp_and_mana_dio = load_image("hp_and_mana_dio.png")
---
+++ main.py
@@ -0,0 +1,329 @@
+import os
+import pygame as pg
+from pygame import time
+from load_sprites import *
+import time as tm
+
+
+def load_image(name):
+    """Функция загрузки изображения из файла"""
+    filename = os.path.join('data', name)
+    try:
+        image = pg.image.load(filename)
+    except pg.error as error:
+        print('Не могу загрузить изображение:', name)
+        raise SystemExit(error)
+    return image
+
+
+class AnimaSprite(pg.sprite.Sprite):
+    """Анимированный спрайт"""
+    def __init__(self, sheet, hero, x, y):
+        super().__init__(all_sprites)
+        self.hero = hero
+        # frames - атрибут класса,
+        # список для хранения последовательности кадров спрайта:
+        self.frames = sheet
+        # Обнуляем номер текущего кадра:
+        self.cur_frame = 0
+        # image - атрибут класса,
+        # в который помещаем текущий кадр:
+        self.image = self.frames[self.cur_frame]
+        # Помещаем прямоугольник с кадром в координаты (x, y):
+        self.rect = self.image.get_rect().move(x, y)
+
+    def update(self):
+        """Смена кадра спрайта"""
+        # Переключаемся на номер следующего кадра,
+        # но так, чтобы не выйти за границы списка frames.
+        # Для этого закольцовываем счёт кадров при помощи операции %:
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        # Меняем кадр - помещаем новый кадр в атрибут image:
+        self.image = self.frames[self.cur_frame]
+
+    def go_right(self):
+        global x_dio, x_jotaro, flag_stop_jotaro
+        if self.hero == 'jotaro':
+            if x_dio > x_jotaro:
+                pg.time.set_timer(jotaro_one_step_event, 630)
+                self.rect.move(self.rect.x + 20, self.rect.y)
+                x_jotaro += 20
+            else:
+                pg.time.set_timer(jotaro_one_step_event, 630)
+                flag_stop_jotaro = True
+        else:
+            pg.time.set_timer(dio_one_step_event, 630)
+            self.rect.move(self.rect.x + 20, self.rect.y)
+            x_dio += 20
+
+    def go_left(self):
+        global x_dio, x_jotaro, flag_stop_dio
+        if self.hero == 'jotaro':
+            pg.time.set_timer(jotaro_one_step_event, 630)
+            self.rect.move(self.rect.x - 20, self.rect.y)
+            x_jotaro -= 20
+        else:
+            if x_jotaro < x_dio:
+                pg.time.set_timer(dio_one_step_event, 630)
+                self.rect.move(self.rect.x - 20, self.rect.y)
+                x_dio -= 20
+            else:
+                pg.time.set_timer(dio_one_step_event, 630)
+                flag_stop_dio = True
+
+    def jump(self):
+        if self.hero == 'jotaro':
+            pg.time.set_timer(jotaro_jump_event, 1695)
+        else:
+            pg.time.set_timer(dio_jump_event, 930)
+
+    def start_sitting(self):
+        if self.hero == 'jotaro':
+            pg.time.set_timer(jotaro_start_sitting_event, 310)
+        else:
+            pg.time.set_timer(dio_start_sitting_event, 235)
+
+    def stand_up(self):
+        if self.hero == 'jotaro':
+            pg.time.set_timer(jotaro_stand_up_event, 772)
+        else:
+            pg.time.set_timer(dio_stand_up_event, 235)
+
+    def light_attack(self):
+        # Все лёгкие атаки Джотаро должны быть в 8 спрайтов
+        if self.hero == 'jotaro':
+            pg.time.set_timer(jotaro_light_attack_event, 630)
+        # Все лёгкие атаки Дио должны быть в 5 спрайтов
+        else:
+            pg.time.set_timer(dio_light_attack_event, 400)
+
+    def block(self):
+        if self.hero == 'jotaro':
+            pg.time.set_timer(jotaro_start_blocking_event, 230)
+        else:
+            pg.time.set_timer(dio_start_blocking_event, 230)
+
+    def end_blocking(self):
+        if self.hero == 'jotaro':
+            pg.time.set_timer(jotaro_end_blocking_event, 700)
+        else:
+            pass
+
+
+class Hp_and_Mana(pg.sprite.Sprite):
+    def __init__(self, sheet, x, y):
+        super().__init__(hp_and_mana_sprites)
+        self.image = sheet
+        self.rect = self.image.get_rect().move(x, y)
+
+
+if __name__ == '__main__':
+    # Инициализируем pygame
+    pg.init()
+    pg.display.set_caption('Jotaro VS Dio')
+    # Задаём размер окна
+    size = width, height = 800, 600
+    # Создаём окно
+    screen = pg.display.set_mode((size))
+    # Создаём группы спрайтов
+    all_sprites, hp_and_mana_sprites = pg.sprite.Group(), pg.sprite.Group()
+    # Задаём спрайты шкал хп и маны
+    hp_and_mana_jotaro = Hp_and_Mana(sprite_hp_and_mana_jotaro, 0, 0)
+    hp_and_mana_dio = Hp_and_Mana(sprite_hp_and_mana_dio, 499, 0)
+    # Задаём event'ы для анимаций персонажейb
+    jotaro_one_step_event, jotaro_jump_event, jotaro_start_sitting_event, jotaro_stand_up_event,\
+        jotaro_light_attack_event, jotaro_start_blocking_event, jotaro_end_blocking_event = pg.USEREVENT + 1, pg.USEREVENT + 2, pg.USEREVENT + 3,\
+        pg.USEREVENT + 4, pg.USEREVENT + 5, pg.USEREVENT + 6, pg.USEREVENT + 7
+    dio_one_step_event, dio_jump_event, dio_start_sitting_event, dio_stand_up_event,\
+        dio_light_attack_event, dio_start_blocking_event, dio_end_blocking_event = pg.USEREVENT + 9, pg.USEREVENT + 10, pg.USEREVENT + 11,\
+        pg.USEREVENT + 12, pg.USEREVENT + 13, pg.USEREVENT + 14, pg.USEREVENT + 15
+    # Задаём координаты отрисовки спрайтов в игровом окне:
+    x_jotaro, y_jotaro, x_dio, y_dio = (0, 380, 660, 356)
+    # Задаём кол-во хп и маны у персонажей
+    hp_jotaro, mana_jotaro, hp_dio, mana_dio = 100, 30, 100, 30
+    # Задаём флаги событий персонажей
+    flag_walking_jotaro, flag_jumping_jotaro, flag_sitting_jotaro, flag_attacking_jotaro, flag_blocking_jotaro, flag_stop_jotaro = \
+        False, False, False, False, False, False
+    flag_walking_dio, flag_jumping_dio, flag_sitting_dio, flag_attacking_dio, flag_blocking_dio, flag_stop_dio = \
+        False, False, False, False, False, False
+    # Создаём экземпляры анимированных спрайтов:
+    sprite_jotaro = AnimaSprite(sprite_jotaro_afk_right_side, 'jotaro', x_jotaro, y_jotaro)
+    sprite_dio = AnimaSprite(sprite_dio_afk_left_side, 'dio', x_dio, y_dio)
+    # Задаём повтор клавиш
+    pg.key.set_repeat(1, 10)
+    # Задаём fps
+    fps = 13
+    # count - переменная, которая считает, на какую ногу должен будет наступать персонаж
+    # (0 - правая, 1 - левая)
+    count_jotaro, count_dio = 0, 0
+    running = True
+    # Главный игровой цикл:
+    while running:
+        if x_jotaro < x_dio:
+            flag_stop_jotaro = False
+            flag_stop_dio = False
+        for event in pg.event.get():
+            keys = pg.key.get_pressed()
+            flags_jotaro = {flag_walking_jotaro, flag_jumping_jotaro, flag_sitting_jotaro, flag_attacking_jotaro}
+            flags_dio = {flag_jumping_dio, flag_sitting_dio, flag_attacking_dio, flag_walking_dio}
+            if event.type == pg.QUIT:
+                running = False
+            elif event.type == pg.KEYDOWN:
+                # Обработка клавиш для Джотаро
+                if keys[pg.K_d] and x_jotaro + 97 <= width and not any(flags_jotaro) and flag_stop_jotaro == False:
+                    sprite_jotaro.kill()
+                    if count_jotaro == 0:
+                        sprite_jotaro = AnimaSprite(sprite_jotaro_walking_right1, 'jotaro', x_jotaro, y_jotaro)
+                    elif count_jotaro == 1:
+                        sprite_jotaro = AnimaSprite(sprite_jotaro_walking_right2, 'jotaro', x_jotaro, y_jotaro)
+                    flag_walking_jotaro = True
+                    count_jotaro = (count_jotaro + 1) % 2
+                    sprite_jotaro.go_right()
+                elif keys[pg.K_a] and x_jotaro - 20 >= 0 and not any(flags_jotaro):
+                    sprite_jotaro.kill()
+                    if count_jotaro == 0:
+                        sprite_jotaro = AnimaSprite(sprite_jotaro_walking_left1, 'jotaro', x_jotaro, y_jotaro)
+                    elif count_jotaro == 1:
+                        sprite_jotaro = AnimaSprite(sprite_jotaro_walking_left2, 'jotaro', x_jotaro, y_jotaro)
+                    flag_walking_jotaro = True
+                    count_jotaro = (count_jotaro + 1) % 2
+                    sprite_jotaro.go_left()
+                elif keys[pg.K_w] and not any(flags_jotaro):
+                    sprite_jotaro.kill()
+                    flag_jumping_jotaro = True
+                    sprite_jotaro = AnimaSprite(sprite_jotaro_jumping, 'jotaro', x_jotaro, y_jotaro)
+                    sprite_jotaro.jump()
+                elif keys[pg.K_s] and not any(flags_jotaro):
+                    sprite_jotaro.kill()
+                    flag_sitting_jotaro = True
+                    sprite_jotaro = AnimaSprite(sprite_jotaro_start_sitting, 'jotaro', x_jotaro, y_jotaro)
+                    sprite_jotaro.start_sitting()
+                elif keys[pg.K_l] and not any(flags_jotaro):
+                    sprite_jotaro.kill()
+                    flag_attacking_jotaro = True
+                    sprite_jotaro = AnimaSprite(sprite_jotaro_light_attack, 'jotaro', x_jotaro, y_jotaro)
+                    sprite_jotaro.light_attack()
+                elif keys[pg.K_l] and flag_sitting_jotaro and not flag_attacking_jotaro:
+                    sprite_jotaro.kill()
+                    flag_attacking_jotaro = True
+                    sprite_jotaro = AnimaSprite(sprite_jotaro_sitting_light_attack, 'jotaro', x_jotaro, y_jotaro)
+                    sprite_jotaro.light_attack()
+                # Обработка клавиш для Дио
+                elif keys[pg.K_LEFT] and not any(flags_dio) and x_dio + 20 >= 0 and flag_stop_dio == False:
+                    sprite_dio.kill()
+                    if count_dio == 0:
+                        sprite_dio = AnimaSprite(sprite_dio_walking_left1, 'dio', x_dio, y_dio)
+                    elif count_dio == 1:
+                        sprite_dio = AnimaSprite(sprite_dio_walking_left2, 'dio', x_dio, y_dio)
+                    flag_walking_dio = True
+                    count_dio = (count_dio + 1) % 2
+                    sprite_dio.go_left()
+                elif keys[pg.K_RIGHT] and not any(flags_dio) and x_dio + 150 <= width:
+                    sprite_dio.kill()
+                    if count_dio == 0:
+                        sprite_dio = AnimaSprite(sprite_dio_walking_right1, 'dio', x_dio, y_dio)
+                    elif count_dio == 1:
+                        sprite_dio = AnimaSprite(sprite_dio_walking_right2, 'dio', x_dio, y_dio)
+                    flag_walking_dio = True
+                    count_dio = (count_dio + 1) % 2
+                    sprite_dio.go_right()
+                elif keys[pg.K_UP] and not any(flags_dio):
+                    sprite_dio.kill()
+                    flag_jumping_dio = True
+                    sprite_dio = AnimaSprite(sprite_dio_jumping, 'dio', x_dio, y_dio)
+                    sprite_dio.jump()
+                elif keys[pg.K_DOWN] and not any(flags_dio):
+                    sprite_dio.kill()
+                    flag_sitting_dio = True
+                    sprite_dio = AnimaSprite(sprite_dio_start_sitting, 'dio', x_dio, y_dio)
+                    sprite_dio.start_sitting()
+                elif keys[pg.K_KP7] and not any(flags_dio):
+                    sprite_dio.kill()
+                    flag_attacking_dio = True
+                    sprite_dio = AnimaSprite(sprite_dio_light_attack, 'dio', x_dio, y_dio)
+                    sprite_dio.light_attack()
+                elif keys[pg.K_KP7] and flag_sitting_dio and not flag_attacking_dio:
+                    sprite_dio.kill()
+                    flag_attacking_dio = True
+                    sprite_dio = AnimaSprite(sprite_dio_sitting_light_attack, 'dio', x_dio, y_dio)
+                    sprite_dio.light_attack()
+            # Обработка нажаний клавиш при отпускании
+            elif event.type == pg.KEYUP:
+                if event.key == pg.K_s and flag_sitting_jotaro:
+                    sprite_jotaro.kill()
+                    sprite_jotaro = AnimaSprite(sprite_jotaro_end_sitting, 'jotaro', x_jotaro, y_jotaro)
+                    sprite_jotaro.stand_up()
+                elif event.key == pg.K_DOWN and flag_sitting_dio:
+                    sprite_dio.kill()
+                    sprite_dio = AnimaSprite(sprite_dio_end_sitting, 'dio', x_dio, y_dio)
+                    sprite_dio.stand_up()
+            # Добавление event'ов для проигрывания анимаций Джотаро
+            elif event.type == jotaro_one_step_event:
+                sprite_jotaro.kill()
+                pg.time.set_timer(jotaro_one_step_event, 0)
+                sprite_jotaro = AnimaSprite(sprite_jotaro_afk_right_side, 'jotaro', x_jotaro, y_jotaro)
+                flag_walking_jotaro = False
+            elif event.type == jotaro_jump_event:
+                sprite_jotaro.kill()
+                pg.time.set_timer(jotaro_jump_event, 0)
+                sprite_jotaro = AnimaSprite(sprite_jotaro_afk_right_side, 'jotaro', x_jotaro, y_jotaro)
+                flag_jumping_jotaro = False
+            elif event.type == jotaro_start_sitting_event:
+                sprite_jotaro.kill()
+                pg.time.set_timer(jotaro_start_sitting_event, 0)
+                sprite_jotaro = AnimaSprite(sprite_jotaro_sitting, 'jotaro', x_jotaro, y_jotaro)
+            elif event.type == jotaro_stand_up_event:
+                sprite_jotaro.kill()
+                pg.time.set_timer(jotaro_stand_up_event, 0)
+                sprite_jotaro = AnimaSprite(sprite_jotaro_afk_right_side, 'jotaro', x_jotaro, y_jotaro)
+                flag_sitting_jotaro = False
+            elif event.type == jotaro_light_attack_event:
+                sprite_jotaro.kill()
+                pg.time.set_timer(jotaro_light_attack_event, 0)
+                if flag_sitting_jotaro:
+                    sprite_jotaro = AnimaSprite(sprite_jotaro_sitting, 'jotaro', x_jotaro, y_jotaro)
+                else:
+                    sprite_jotaro = AnimaSprite(sprite_jotaro_afk_right_side, 'jotaro', x_jotaro, y_jotaro)
+                flag_attacking_jotaro = False
+            # Добавление event'ов для проигрывания анимаций Дио
+            elif event.type == dio_one_step_event:
+                sprite_dio.kill()
+                pg.time.set_timer(dio_one_step_event, 0)
+                sprite_dio = AnimaSprite(sprite_dio_afk_left_side, 'dio', x_dio, y_dio)
+                flag_walking_dio = False
+            elif event.type == dio_jump_event:
+                sprite_dio.kill()
+                pg.time.set_timer(dio_jump_event, 0)
+                sprite_dio = AnimaSprite(sprite_dio_afk_left_side, 'dio', x_dio, y_dio)
+                flag_jumping_dio = False
+            elif event.type == dio_start_sitting_event:
+                sprite_dio.kill()
+                pg.time.set_timer(dio_start_sitting_event, 0)
+                sprite_dio = AnimaSprite(sprite_dio_sitting, 'dio', x_dio, y_dio)
+            elif event.type == dio_stand_up_event:
+                sprite_dio.kill()
+                pg.time.set_timer(dio_stand_up_event, 0)
+                sprite_dio = AnimaSprite(sprite_dio_afk_left_side, 'dio', x_dio, y_dio)
+                flag_sitting_dio = False
+            elif event.type == dio_light_attack_event:
+                sprite_dio.kill()
+                pg.time.set_timer(dio_light_attack_event, 0)
+                if flag_sitting_dio:
+                    sprite_dio = AnimaSprite(sprite_dio_sitting, 'dio', x_dio, y_dio)
+                else:
+                    sprite_dio = AnimaSprite(sprite_dio_afk_left_side, 'dio', x_dio, y_dio)
+                flag_attacking_dio = False
+        screen.fill(pg.Color('white'))
+        all_sprites.draw(screen)
+        all_sprites.update()
+        hp_and_mana_sprites.draw(screen)
+        hp_and_mana_dio.update()
+        # Отрисовка здоровья и маны
+        pg.draw.rect(screen, pg.Color('red'), (0, 21, hp_jotaro * 3, 5))
+        pg.draw.rect(screen, pg.Color('blue'), (0, 26, mana_jotaro * 3, 4))
+        pg.draw.rect(screen, pg.Color('red'), (800 - hp_dio * 3, 21, 800, 5))
+        pg.draw.rect(screen, pg.Color('blue'), (800 - mana_dio * 3, 26, 800, 4))
+        pg.draw.rect(screen, pg.Color('black'), (0, 530, 800, 1))
+        pg.display.flip()
+        time.Clock().tick(fps)
+    pg.quit()
